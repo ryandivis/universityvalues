ea706c2a599c42d5b8f7ddd2c2c119aeb17eefb7
ryan <ryandivis@yahoo.com>
2013-10-28 22:34:18 -0600

    changes to mysql.php

--- includes/mysql.php
+++ includes/mysql.php
@@ -3,13 +3,16 @@
 error_reporting(0);
 
 if($_SERVER['HTTP_HOST']=='universityvalues.com' || $_SERVER['HTTP_HOST']=='www.universityvalues.com')
-
+{
 	$mysql = mysql_connect("localhost","onlinea6_uv","warner2009");
 
+	mysql_select_db("onlinea6_uv");
+}
 else
-
+{
 	$mysql = mysql_connect("localhost","root","");
 
 	mysql_select_db("uv", $mysql);
+}
 
 ?>
